453bca4ac669c90b0652fcbe430a0ef017f4af9c
franderms <franderms@gmail.com>
2025-01-09 23:18:34 -0600

    Challenge 8 completed, 4 star achieved

---
+++ challenge8/challenge8.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} indices - The reno indices
+ * @param {number} length - The length of the race
+ * @returns {string} The reno race
+ */
+function drawRace(indices, length) {
+    const shifts = indices.length - 1
+    const lanesArray = indices.map((indexReinder, index) => {
+        const indexR = (length + indexReinder)%length
+        const laneArray = new Array(length).fill(`~`)
+        const fill = new Array(shifts - index).fill(' ')
+        if(indexR !== 0) laneArray[indexR] = 'r';
+        laneArray.push(` /${index+1}`)
+        laneArray.unshift(...fill)
+        return laneArray.join('')
+    })
+    return lanesArray.join('\n')
+}
+//console.log(drawRace([0, 5, -3], 10))
+
+//console.log(drawRace([2, -1, 0, 5], 8))
+
+console.log(drawRace([3, 7, -2], 12))
