4ef160e8182a7aa64938f7b6c27f329585306152
franderms <franderms@gmail.com>
2025-01-15 23:48:34 -0600

    Challenge 13 semi-completed, just left one test to pass but I still cant find the corner casse where the program fails

---
+++ challenge 13/challenge13.js
@@ -0,0 +1,66 @@
+/** 
+ * @param {string} moves
+ * @returns {true|[number, number]} Return true if robot returns or position
+ */
+function isRobotBack(moves) {
+  // Code here
+  const movesVector = {
+    U: [0,1],
+    D: [0,-1],
+    L: [-1,0],
+    R: [1,0]
+  }
+  const invertMovs = {
+    U: 'D',
+    D: 'U',
+    L: 'R',
+    R: 'L'
+  }
+  const movRegister = new Set()
+  const origin = [0,0]
+  let lastSimbol = ''
+  for(let mov of moves){
+    if(!movesVector[mov]){
+      lastSimbol = mov
+      continue
+    } 
+    let movToRegist = mov
+    let vectorToSum = movesVector[mov]
+    if(lastSimbol === '*'){
+      vectorToSum[0] *= 2
+      vectorToSum[1] *= 2
+    }
+    if(lastSimbol === '!'){
+      const invertMov = invertMovs[mov]
+      vectorToSum = movesVector[invertMov]
+      movToRegist = invertMov
+    }
+    if(lastSimbol === '?'){
+      vectorToSum = movRegister.has(mov) ? [0,0] : vectorToSum
+    }
+    origin[0] += vectorToSum[0]
+    origin[1] += vectorToSum[1]
+    movRegister.add(movToRegist)
+    lastSimbol = mov
+  }
+  return (origin[0] === 0 && origin[1] === 0) || origin
+}
+console.log(isRobotBack('!R'))
+// console.log(isRobotBack(''))
+// console.log(isRobotBack('R'))     // [1, 0]
+// console.log(isRobotBack('RL'))    // true
+// console.log(isRobotBack('RLUD') ) // true
+// console.log(isRobotBack('*RU'))   // [2, 1]
+// console.log(isRobotBack('R*U'))   // [1, 2]
+// console.log(isRobotBack('LLL!R')) // [-4, 0]
+// console.log(isRobotBack('R?R'))   // [1, 0]
+// console.log(isRobotBack('U?D'))   // true
+// console.log(isRobotBack('R!L'))   // [2,0]
+// console.log(isRobotBack('U!D'))   // [0,2]
+// console.log(isRobotBack('R?L'))   // true
+// console.log(isRobotBack('U?U'))   // [0,1]
+// console.log(isRobotBack('*U?U'))  // [0,2]
+// console.log(isRobotBack('U?D?U?')) // true
+
+// console.log(isRobotBack('R!U?U'))
+// console.log(isRobotBack('UU!U?D'))
