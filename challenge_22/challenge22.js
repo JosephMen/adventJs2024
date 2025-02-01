63e022e4f79508c68dd5338faee1697b30c7554a
franderms <franderms@gmail.com>
2025-02-01 01:06:21 -0600

    Challenge 22 completed, 5 stars achieved, such hard challenge !!

--- challenge_22/challenge22.js
+++ challenge_22/challenge22.js
@@ -4,15 +4,24 @@
  */
 function generateGiftSets(gifts) {
 	// Code here
-	const backTrack = (indexes, acc) => {
-		acc.push(indexes.map(i => gifts[i]))
-		for(let i = indexes.length - 1; i >= 0; i--){
+	const numOfGifts = gifts.length
 
+	const backTrack = (lengthGr, indexStart, subGroup, acc) => {
+		if (lengthGr === 0) return acc.push(subGroup)
+		for(let i = 0; i + indexStart < numOfGifts; i++){
+			const extendedGroup = [...subGroup, gifts[indexStart + i]]
+			backTrack(lengthGr - 1, indexStart + 1 + i, extendedGroup, acc)
 		}
 	}
 	const result = []
-	for(let i = 1; i < gifts.length; i++){
-
+	for(let i = 1; i <= numOfGifts; i++){
+		backTrack(i, 0, [], result)
 	}
-	return [].fill()
+	return result
 }
+const result1 = generateGiftSets(['car', 'doll', 'puzzle'])
+console.log(result1)
+
+console.log(generateGiftSets(['ball']))
+
+console.log(generateGiftSets(['game', 'pc']))
